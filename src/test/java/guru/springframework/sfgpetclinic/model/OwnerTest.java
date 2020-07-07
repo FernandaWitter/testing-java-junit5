41e4ca59fc7b1b274cb9f7e9774efed2edb74900
Fernanda I B Witter <fernanda.witter@yahoo.com>
2020-07-07 10:23:10 -0300

    feat: implements dependent assertion test

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,23 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+
+    @Test
+    public void dependentAssertions(){
+        Owner owner = new Owner(1L, "John", "Smith");
+        owner.setCity("Cardiff");
+        owner.setTelephone("123-TARDIS");
+
+        assertAll("Properties Test",
+                () -> assertAll("Person Properties",
+                        ()-> assertEquals("John", owner.getFirstName(), "First name did not match"),
+                        ()-> assertEquals("Smith", owner.getLastName(), "Last name did not match")),
+                () -> assertAll("Owner Properties",
+                        () -> assertEquals("Cardiff", owner.getCity(), "City did not match"),
+                        () -> assertEquals("123-TARDIS", owner.getTelephone(), "Phone did not match")));
+    }
+}

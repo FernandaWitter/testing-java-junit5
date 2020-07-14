ac5de7953252e0405e91060905bd845cdb515479
Fernanda I B Witter <fernanda.witter@yahoo.com>
2020-07-14 10:39:32 -0300

    feat: implements parameterized test with csv file source example

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -4,6 +4,7 @@ import guru.springframework.sfgpetclinic.ModelTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -46,6 +47,13 @@ class OwnerTest implements ModelTest {
     @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
     @CsvSource({"FL,1,1", "OH,2,2", "MI,3,3"})
     void csvInputTest(String stateName, int val1, int val2){
+        System.out.println(stateName + " = " + val1 + " : " + val2);
+    }
+
+    @DisplayName("CSV from File Input Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
+    void csvInputFromFileTest(String stateName, int val1, int val2){
         System.out.println(stateName + " = " + val1 + " : " + val2);
     }
 }

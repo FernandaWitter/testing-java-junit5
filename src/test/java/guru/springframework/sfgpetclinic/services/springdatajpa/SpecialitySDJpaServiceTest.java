ff734bf82a7c172b14f3b99655398ae10d3636ae
Fernanda I B Witter <fernanda.witter@yahoo.com>
2020-07-17 15:46:20 -0300

    feat: implements interaction verification examples

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -20,6 +21,34 @@ class SpecialitySDJpaServiceTest {
     @Test
     void deleteById() {
         specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+
+        Mockito.verify(specialtyRepository, Mockito.times(2)).deleteById(1L);
+    }
+
+    @Test
+    void deleteByIdAtLeast() {
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+
+        Mockito.verify(specialtyRepository, Mockito.atLeastOnce()).deleteById(1L);
+        Mockito.verify(specialtyRepository, Mockito.atLeast(2)).deleteById(1L);
+    }
+
+    @Test
+    void deleteByIdAtMost() {
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+
+        Mockito.verify(specialtyRepository, Mockito.atMost(5)).deleteById(1L);
+    }
+
+    @Test
+    void deleteByIdNever() {
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+
+        Mockito.verify(specialtyRepository, Mockito.never()).deleteById(5L);
     }
 
     @Test

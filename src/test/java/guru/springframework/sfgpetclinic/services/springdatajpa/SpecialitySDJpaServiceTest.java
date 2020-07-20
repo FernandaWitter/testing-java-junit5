d1357f93e84529cd868f8d972914f11f7e6e39f1
Fernanda I B Witter <fernanda.witter@yahoo.com>
2020-07-20 10:38:59 -0300

    feat: implements BDDMockito alternatives to SpecialtySDJpaService tests

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -30,8 +30,8 @@ class SpecialitySDJpaServiceTest {
 
     @Test
     void findByIdBddTest(){
-        Speciality speciality = new Speciality();
         // Given
+        Speciality speciality = new Speciality();
         BDDMockito.given(specialtyRepository.findById(1L)).willReturn(Optional.of(speciality));
         // When
         Speciality foundSpecialty = specialitySDJpaService.findById(1L);
@@ -50,11 +50,32 @@ class SpecialitySDJpaServiceTest {
     }
 
     @Test
+    void deleteByObjectBddTest(){
+        // Given
+        Speciality speciality = new Speciality();
+        // When
+        specialitySDJpaService.delete(speciality);
+        // Then
+        BDDMockito.then(specialtyRepository).should().delete(ArgumentMatchers.any(Speciality.class));
+    }
+
+    @Test
     void deleteById() {
         specialitySDJpaService.deleteById(1L);
         specialitySDJpaService.deleteById(1L);
 
         Mockito.verify(specialtyRepository, Mockito.times(2)).deleteById(1L);
+    }
+
+    @Test
+    void deleteByIdBddTest() {
+        // Given - none
+
+        // When
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+        // Then
+        BDDMockito.then(specialtyRepository).should(Mockito.times(2)).deleteById(1L);
     }
 
     @Test
@@ -67,6 +88,18 @@ class SpecialitySDJpaServiceTest {
     }
 
     @Test
+    void deleteByIdAtLeastBddTest() {
+        // Given
+
+        // When
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+        // Then
+        BDDMockito.then(specialtyRepository).should(Mockito.atLeastOnce()).deleteById(1L);
+        BDDMockito.then(specialtyRepository).should(Mockito.atLeast(2)).deleteById(1L);
+    }
+
+    @Test
     void deleteByIdAtMost() {
         specialitySDJpaService.deleteById(1L);
         specialitySDJpaService.deleteById(1L);
@@ -75,16 +108,48 @@ class SpecialitySDJpaServiceTest {
     }
 
     @Test
+    void deleteByIdAtMostBddTest() {
+        // Given
+
+        // When
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+        // Then
+        BDDMockito.then(specialtyRepository).should(Mockito.atMost(5)).deleteById(1L);
+    }
+
+    @Test
     void deleteByIdNever() {
         specialitySDJpaService.deleteById(1L);
         specialitySDJpaService.deleteById(1L);
 
+        Mockito.verify(specialtyRepository, Mockito.atLeastOnce()).deleteById(1L);
         Mockito.verify(specialtyRepository, Mockito.never()).deleteById(5L);
+    }
+
+    @Test
+    void deleteByIdNeverBddTest() {
+        // Given
+
+        // When
+        specialitySDJpaService.deleteById(1L);
+        specialitySDJpaService.deleteById(1L);
+        // Then
+        BDDMockito.then(specialtyRepository).should(Mockito.atLeastOnce()).deleteById(1L);
+        BDDMockito.then(specialtyRepository).should(Mockito.never()).deleteById(5L);
     }
 
     @Test
     void testDelete(){
         specialitySDJpaService.delete(new Speciality());
+    }
+
+    @Test
+    void testDeleteBdd(){
+        // When
+        specialitySDJpaService.delete(new Speciality());
+        // Then
+        BDDMockito.then(specialtyRepository).should().delete(ArgumentMatchers.any(Speciality.class));
     }
 
 }
